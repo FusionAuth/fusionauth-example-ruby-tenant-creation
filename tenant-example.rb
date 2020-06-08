99ada0fa476103f975a052acc8b8e14083ab28fd
Dan Moore <github@mooreds.com>
2020-06-08 18:56:01 +0000

    Broke api calls out to methods.

--- tenant-example.rb
+++ tenant-example.rb
@@ -1,107 +1,136 @@
 require 'fusionauth/fusionauth_client'
 require 'pp'
 
-time = Time.now.getutc.to_s
+def create_new_tenant(generic_tenant_client, name)
+  # create a new tenant
+  tenants_response = generic_tenant_client.retrieve_tenants
+  #PP.pp tenants.success_response.tenants.select { |t| t.name == 'Default' }[0].id
+  if tenants_response.status != 200
+    puts "Unable to retrieve tenants."
+    return
+  end
+  
+  default_tenant_id = tenants_response.success_response.tenants.select { |t| t.name == 'Default' }[0].id
+  default_tenant_theme_id = tenants_response.success_response.tenants.select { |t| t.name == 'Default' }[0].themeId
+  
+  new_tenant_request = { "sourceTenantId": default_tenant_id, tenant: {"name": "New client - "+name }}
+  new_tenant_response = generic_tenant_client.create_tenant(nil, new_tenant_request)
 
-generic_tenant_client = FusionAuth::FusionAuthClient.new(
-      ENV['API_KEY'],
-      'http://localhost:9011'
-    )
+  if new_tenant_response.status != 200
+    puts "Unable to create tenant."
+    puts new_tenant_response.error_response
+    return false
+  end
+  new_tenant = new_tenant_response.success_response.tenant
+  [default_tenant_theme_id, new_tenant]
+end
 
-# create a new tenant
-tenants_response = generic_tenant_client.retrieve_tenants
-#PP.pp tenants.success_response.tenants.select { |t| t.name == 'Default' }[0].id
-if tenants_response.status != 200
-  puts "Unable to retrieve tenants."
+def create_application(new_tenant_id, name)
+  # used for making requests scoped to this tenant
+  new_tenant_client = FusionAuth::FusionAuthClient.new(ENV['API_KEY'], FA_HOST)
+  new_tenant_client.set_tenant_id(new_tenant_id)
+  
+  # create an application in that tenant
+  new_application_request = { application: {"name": "New client app - "+name }}
+  new_application_response = new_tenant_client.create_application(nil, new_application_request)
+  
+  if new_application_response.status != 200
+    puts "Unable to create application."
+    puts new_application_response.error_response
+    return false
+  end
+  true
+end
+
+def create_new_theme(generic_tenant_client, name, default_tenant_theme_id)
+  # create new theme
+  new_theme_request = { "sourceThemeId": default_tenant_theme_id, theme: {"name": "New theme - "+name }}
+  new_theme_response = generic_tenant_client.create_theme(nil, new_theme_request)
+  if new_theme_response.status != 200
+    puts "Unable to create new theme."
+    puts new_theme_response.error_response
+    return false
+  end
+  new_theme_response.success_response.theme
+end
+
+def update_theme(generic_tenant_client, theme)
+  templates = theme.templates
+  # modify the title tag
+  templates.helpers = IO.read(File.new("helpers"))
+
+  # convert template object to hash
+  keys = templates.to_h.keys
+  t_body = {}
+  keys.each { |k| t_body[k.to_s] = templates.to_h[k] }
+  patch_theme_request = { theme: {templates: t_body}}
+  patch_theme_response = generic_tenant_client.patch_theme(theme.id, patch_theme_request)
+
+  if patch_theme_response.status != 200
+    #PP.pp update_theme_response
+    puts "Unable to patch theme."
+    puts patch_theme_response.error_response
+    return false
+  end
+
+  true
+end
+
+def update_tenant_with_new_theme(generic_tenant_client, new_tenant_id, theme_id)
+
+  # update tenant with new theme
+
+  patch_tenant_theme_request = { tenant: {themeId: theme_id} }
+  patch_tenant_theme_response = generic_tenant_client.patch_tenant(new_tenant_id, patch_tenant_theme_request)
+  
+  if patch_tenant_theme_response.status != 200
+    puts "Unable to update tenant to new theme."
+    puts patch_tenant_theme_response.error_response
+    return false
+  end
+  true
+end
+
+# start the actual process
+#
+# each method returns false unless it succeeds
+
+NAME = "name4"
+FA_HOST = "http://localhost:9011"
+
+generic_tenant_client = FusionAuth::FusionAuthClient.new(ENV['API_KEY'], FA_HOST)
+
+result = create_new_tenant(generic_tenant_client, NAME)
+
+unless result
   return
 end
 
-default_tenant_id = tenants_response.success_response.tenants.select { |t| t.name == 'Default' }[0].id
-default_tenant_theme_id = tenants_response.success_response.tenants.select { |t| t.name == 'Default' }[0].themeId
+default_tenant_theme_id, new_tenant = result
 
-new_tenant_request = { "sourceTenantId": default_tenant_id, tenant: {"name": "New client "+time }}
-new_tenant_response = generic_tenant_client.create_tenant(nil, new_tenant_request)
+result = create_application(new_tenant.id, NAME)
 
-if new_tenant_response.status != 200
-  puts "Unable to create tenant."
+unless result
   return
 end
 
-new_tenant = new_tenant_response.success_response.tenant
-
-# used for making requests scoped to this tenant
-new_tenant_client = FusionAuth::FusionAuthClient.new(
-      ENV['API_KEY'],
-      'http://localhost:9011'
-    )
-new_tenant_client.set_tenant_id(new_tenant.id)
-
-# create an application in that tenant
-new_application_request = { application: {"name": "New client app "+time }}
-new_application_response = new_tenant_client.create_application(nil, new_application_request)
-
-if new_application_response.status != 200
-  puts "Unable to create application."
+result = create_new_theme(generic_tenant_client, NAME, default_tenant_theme_id)
+unless result
   return
 end
 
-# create new theme
-new_theme_request = { "sourceThemeId": default_tenant_theme_id, theme: {"name": "New theme "+time }}
-new_theme_response = generic_tenant_client.create_theme(nil, new_theme_request)
-if new_theme_response.status != 200
-  puts "Unable to create new theme."
+theme = result
+
+result = update_theme(generic_tenant_client, theme)
+unless result
   return
 end
 
-# update new theme to helpers from this repo
-theme = new_theme_response.success_response.theme
-# msgs = theme.defaultMessages
-#puts msgs
+result = update_tenant_with_new_theme(generic_tenant_client, new_tenant.id, theme.id)
 
-#keys = msgs.to_h.keys
-#m_body = {}
-#keys.each { |k| m_body[k.to_s] = msgs.to_h[k] }
-
-
-templates = theme.templates
-#puts templates.to_h.keys.length
-#puts templates.helpers
-
-# modify the title tag
-templates.helpers = IO.read(File.new("helpers"))
-
-keys = templates.to_h.keys
-t_body = {}
-keys.each { |k| t_body[k.to_s] = templates.to_h[k] }
-patch_theme_request = { theme: {templates: t_body}}
-#puts update_theme_request.to_json
-#puts theme.id
-#puts patch_theme_request 
-patch_theme_response = generic_tenant_client.patch_theme(theme.id, patch_theme_request)
-
-if patch_theme_response.status != 200
-  #PP.pp update_theme_response
-  puts "Unable to update theme."
-  puts patch_theme_response.error_response
-  return
-end
-
-# update tenant with new theme
-
-patch_tenant_theme_request = { tenant: {themeId: theme.id} }
-patch_tenant_theme_response = generic_tenant_client.patch_tenant(new_tenant.id, patch_tenant_theme_request)
-
-if patch_tenant_theme_response.status != 200
-  puts "Unable to update tenant to new theme."
-  puts patch_tenant_theme_response.error_response
+unless result
   return
 end
 
 puts "Success. Enjoy your new tenant."
 
-#puts theme_response.success_response.theme.templates.to_h.keys
-
-# change something
-# push the theme
-# set the theme for the new tenant
-# TODO put into methods
